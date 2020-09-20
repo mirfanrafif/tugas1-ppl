20ab7d6bbe77380c885fb1e947c4d920b0193cd6
afif hendrawan <afif.hendrawan@gmail.com>
2020-09-20 22:55:21 +0700

    complete solution

--- src/app.js
+++ src/app.js
@@ -1,13 +1,35 @@
-import { incrementQty, decrementQty } from './helpers.js';
+import { 
+  incrementQty, 
+  decrementQty,
+  recalculateSubTotal 
+} from './helpers.js';
 
 const incrButton = document.querySelector('#incr');
 const decrButton = document.querySelector('#decr');
 const qtyInput = document.querySelector('#qty');
+const price = document.querySelector('#price');
+const subtotal = document.querySelector('#subtotal');
+const code = document.querySelector('#code');
+const confirmButton = document.querySelector('#confirmButton');
+
+let discount = 0;
 
 incrButton.addEventListener('click', () => {
   qtyInput.value = incrementQty(qtyInput.value);
+  subtotal.textContent = `Rp. ${recalculateSubTotal(price.value, qtyInput.value, discount)}`;
 });
 
 decrButton.addEventListener('click', () => {
   qtyInput.value = decrementQty(qtyInput.value);
+  subtotal.textContent = `Rp. ${recalculateSubTotal(price.value, qtyInput.value, discount)}`;
+});
+
+confirmButton.addEventListener('click', () => {
+  if (code.value === 'PROMO25') {
+    discount = 25;
+    subtotal.textContent = `Rp. ${recalculateSubTotal(price.value, qtyInput.value, discount)}`
+  } else {
+    discount = 0;
+    subtotal.textContent = `Rp. ${recalculateSubTotal(price.value, qtyInput.value, discount)}`
+  }
 });
--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,14 @@
-const incrementQty = (qty) => qty + 1;
-const decrementQty = (qty) => qty - 1;
-
-export { incrementQty, decrementQty };
+const incrementQty = (qty) => parseInt(qty) + 1;
+const decrementQty = (qty) => {
+    if (qty > 1)
+        return parseInt(qty) - 1;
+    return 1
+};
+const recalculateSubTotal = (price, qty, discount = 0) => {
+    return (1 - parseFloat(discount) / 100) * price * qty;
+}
+module.exports = { 
+    incrementQty, 
+    decrementQty,
+    recalculateSubTotal
+};
